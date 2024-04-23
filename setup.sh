b2f8a4f0ed030f6b279f3b8f51bcbc14d5a967b1
Kreato <kreato@kreato.dev>
2024-04-23 22:52:15 +0300

    feat: mostly finish setup.sh, add lock-sway script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,92 @@
 #!/bin/sh
-# TODO: font check
-# TODO: remove before linking
-# TODO: confirmation prompt
-
-
 cyan='\033[0;36m'
+red='\033[0;31m'
 yellow='\033[0;33m'
 reset='\033[0m'
 
 info() {
-    printf "${cyan}info${reset}: $@"
+    if [ -n "$2" ]; then
+       printf "${cyan}info${reset}: $@"
+    else
+       printf "${cyan}info${reset}: $@\n"
+    fi
 }
 
 warn() {
-    printf "${yellow}warn${reset}: $1 not installed, config may not work properly"
+    printf "${yellow}warn${reset}: $1 is not installed, config may not work properly"
 }
+
+err() {
+    printf "${red}error${reset}: $@\n"
+    exit 1
+}
+
+lnk() {
+    if [ "$REMOVEBEFOREHAND" = "1" ]; then
+       info "Removing $2"
+       rm -rf "$2"
+    fi
+
+    if [ -e "$2" ]; then
+       err "$2 already exists in the filesystem, cannot continue. Run with -r to override this."
+    fi
+
+    ln -s "$1" "$2"
+}
+
+info "This will install Kreato's dotfiles using symbolic links. You CANNOT change the path of the dotfiles repository after continuing. Do you want to proceed? (Y/n) " " "
+
+read -r proceed
+
+case $proceed in
+    "y" | "Y" | "yes" | "Yes")
+       info "Proceeding..."
+    ;;
+    *)
+       info "Closing."
+       exit
+    ;;
+esac
+
+if [ "$1" = "-r" ]; then
+    export REMOVEBEFOREHAND="1"
+else
+    export REMOVEBEFOREHAND="0"
+fi
+
 
 info "Kreato's Dotfiles setup script"
 
-command -v sway >/dev/null 2>&1 && info "sway installed" || warn "sway"
-command -v dunst >/dev/null 2>&1 && info "dunst installed" || warn "dunst"
-command -v nvim  >/dev/null 2>&1 && info "neovim installed" || warn "neovim"
-command -v waybar  >/dev/null 2>&1 && info "waybar installed" || warn "waybar"
-command -v rofi  >/dev/null 2>&1 && info "rofi installed" || warn "rofi" 
+info "Checking dependencies"
 
-cd main
+for i in sway swaylock brightnessctl dunst nvim waybar rofi wal jq; do
+    (command -v $i >/dev/null 2>&1 && info "$i installed") || warn "$i"
+done
+
+if ! (fc-list | grep "NotoSans Nerd Font,NotoSans NF:style=Regular" >/dev/null 2>&1); then
+    warn "NotoSans Nerd Font"
+fi
+
+info "Installing main dotfiles"
+
+cd main || err "please enter source directory before continuing"
 
 mkdir -p "$HOME/.config/dunst"
 
 for i in *; do
-    ln -s "$PWD/$i" "$HOME"/.config/"$i"
+    lnk "$PWD/$i" "$HOME"/.config/"$i"
 done
 
-ln -s "$HOME/.cache/wal/dunstrc" "$HOME/.config/dunst/dunstrc"
+lnk "$HOME/.cache/wal/dunstrc" "$HOME/.config/dunst/dunstrc"
+
+cd ..
+
+info "Installing scripts"
+
+cd scripts || err "unknown error"
+
+mkdir -p "$HOME"/.local/bin
+
+for i in *; do
+    lnk "$PWD/$i" "$HOME"/.local/bin/"$i"
+done
