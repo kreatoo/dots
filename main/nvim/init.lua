26e9b54e913e75e05daff47f158c9b2d92a27bf4
Kreato <kreato@kreato.dev>
2024-05-12 18:12:05 +0300

    sum changes

--- main/nvim/init.lua
+++ main/nvim/init.lua
@@ -11,6 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
 vim.o.termguicolors=true
 vim.o.updatetime = 300
